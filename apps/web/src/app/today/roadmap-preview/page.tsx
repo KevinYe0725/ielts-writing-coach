8c63465e71772ce1539725f6da2c628c7f0c0f7e
Kevin Ye <183179439+KevinYe0725@users.noreply.github.com>
2026-09-12 22:46:07 +0800

    revert: remove roadmap preview experiments

--- apps/web/src/app/today/roadmap-preview/page.tsx
+++
@@ -1,8 +0,0 @@
-import { notFound } from "next/navigation";
-
-import { CourseRoadmapPreview } from "./roadmap-preview";
-
-export default function RoadmapPreviewPage() {
-  if (process.env.NEXT_PUBLIC_DEMO_MODE !== "true") notFound();
-  return <CourseRoadmapPreview />;
-}
